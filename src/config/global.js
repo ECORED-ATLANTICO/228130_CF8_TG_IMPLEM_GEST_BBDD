5ee31b9ca42c8fe20f4a995e2858102e4650a261
Fabio Fonseca <fabiofonseca.arg@gmail.com>
2026-02-26 19:03:32 -0500

    feat: Add material download link to global config, update material archive, and remove old documentation.

--- src/config/global.js
+++ src/config/global.js
@@ -83,6 +83,11 @@ export default {
         nombreRuta: 'referencias',
       },
       {
+        icono: 'fas fa-download',
+        titulo: 'Descargar material',
+        download: 'downloads/material.zip',
+      },
+      {
         icono: 'far fa-registered',
         titulo: 'Créditos',
         nombreRuta: 'creditos',
